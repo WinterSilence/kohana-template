ac0ae1abe080edd5e36ac7f0e1cccb81f642ae00
Anton <antonfedonjuk@gmail.com>
2018-12-15 05:53:28 +0300

    Update init.php

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
+<?php
 
 // Autoloader for vendor classes
 include_once __DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload'.EXT;
