cf52710871e8a823f4b50a7fa31720eb43c2f74e
Anton <antonfedonjuk@gmail.com>
2020-04-12 12:21:51 +0300

    Update Native.php

--- classes/Kohana/Tpl/Native.php
+++ classes/Kohana/Tpl/Native.php
@@ -1,80 +1,72 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
+<?php
 /**
  * Tpl driver for native template engine.
  *
- * @package    Tpl
- * @category   Driver
- * @author     WinterSilence <info@handy-soft.ru>
- * @copyright  2013 © handy-soft.ru
+ * @package    Kohana\Template
+ * @category   View
+ * @author     WinterSilence <info@ensostudio.ru>
+ * @copyright  2013-2020 © Enso studio
  * @license    MIT
- * @link       http://github.com/WinterSilence/kohana-tpl
  */
-abstract class Kohana_Tpl_Native implements Kohana_Tpl_Interface {
-
+abstract class Kohana_Tpl_Native implements Kohana_Tpl_Adapter
+{
 	/**
-	 * @var  string  extension of view file
+	 * @var string extension of view file
 	 */
-	protected $_extension;
+	protected $extension;
 
 	/**
-	 * Create driver for template engine.
-	 * 
-	 * @param   array  $config  engine settings
-	 * @return  void
+	 * @inheritDoc
 	 */
 	public function __construct(array $config)
 	{
-		$this->_extension = $config['extension'];
+		$this->extension = $config['extension'];
 	}
 
 	/**
-	 * Renders the view object to a string.
+	 * Rendering.
 	 * 
-	 * @param   string  $kohana_tpl_file  template file
-	 * @param   array   $kohana_tpl_data  template variables 
-	 * @return  string
+	 * @param string $kohana_tpl_file template file
+	 * @param array $kohana_tpl_data template variables 
+	 * @return string
 	 */
-	private function _render($kohana_tpl_file, array $kohana_tpl_data)
+	protected static function capture(string $kohana_tpl_file, array $kohana_tpl_data): string
 	{
-		// Import the view variables to local namespace
+		// import the view variables to local namespace
 		extract($kohana_tpl_data, EXTR_SKIP);
-		// Capture the view output
+		unset($kohana_tpl_data);
+		// capture the view output
 		ob_start();
-		try
-		{
-			// Load the view within the current scope
+		try {
+			// load the view within the current scope
 			include $kohana_tpl_file;
-		}
-		catch (Exception $e)
-		{
-			// Delete the output buffer
+		} catch (Throwable $e) {
+			// delete the output buffer
 			ob_end_clean();
-			// Re-throw the exception
-			throw $e;
+			// re-throw the exception
+			throw new View_Exception(
+				'Error render template :tpl', 
+				[':tpl' => Debug::path($kohana_tpl_file)], 
+				0, 
+				$e
+			);
 		}
-		// Get the captured output and close the buffer
+		// get the captured output and close the buffer
 		return ob_get_clean();
 	}
 
 	/**
-	 * Renders the template object to a string.
-	 * 
-	 * @param   string  $file  template file
-	 * @param   array   $data  template variables
-	 * @return  string
+	 * @inheritDoc
 	 */
-	public function render($file, array $data)
+	public function render(string $template, array $data): string
 	{
-		// Find template
-		$path = Kohana::find_file('views', $file, $this->_extension);
-		if ($path === NULL)
-		{
-			throw new View_Exception('The requested template :file.:ext could not be found', array(
-				':file' => $file,
-				':ext'  => $this->_extension
-			));
+		$file = Kohana::find_file('views', $template, $this->extension);
+		if (! $file) {
+			throw new View_Exception(
+				'Template file :tpl.:ext not found.',
+				[':tpl' => $template, ':ext' => $this->extension]
+			);
 		}
-		return $this->_render($path, $data);
+		return static::capture($file, $data);
 	}
-
-} // End Tpl_Native
+}
