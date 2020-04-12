0db278673ae7afd0d29b0176254d8ab3c6423548
Anton <antonfedonjuk@gmail.com>
2020-04-12 10:11:42 +0300

    Update init.php

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
 <?php
 
-// Autoloader for vendor classes
-include_once __DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload'.EXT;
+// Composer class loader
+include_once __DIR__ . '/vendor/autoload.php';
